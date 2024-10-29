fb7d93fef9b1aa2e2807b5f940140199a466a961
gerson <gerson.freire@gmail.com>
2024-10-29 11:47:42 -0300

    Refactor bot core initialization and configuration to load bot token from config.yml

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 import os
 from dotenv import load_dotenv
+import yaml
 from bot import TelegramBotFramework
 
 def main():
-    # Load environment variables
-    load_dotenv()
-    
-    # Get bot token from environment but overwrite it if it is provided inside .env file
-    load_dotenv(override=True)
-    bot_token = os.getenv("DEFAULT_BOT_TOKEN")
+    # Load configuration from config.yml
+    config_path = os.path.join(os.path.dirname(__file__), 'config.yml')
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    # Get bot token from config.yml
+    bot_token = config.get('bot')['default_bot_token']
     if not bot_token:
-        raise ValueError("DEFAULT_BOT_TOKEN not found in environment variables")
+        raise ValueError("DEFAULT_BOT_TOKEN not found in config.yml")
 
     # Initialize and run the bot
     bot = TelegramBotFramework(bot_token)
