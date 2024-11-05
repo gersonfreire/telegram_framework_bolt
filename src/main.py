6d927f5c1745fede6a6e5dccf08cbc8ebaf239c3
gerson <gerson.freire@gmail.com>
2024-11-05 10:28:31 -0300

    Update main.py to allow bot token to be set from environment variable with override option

--- src/main.py
+++ src/main.py
@@ -4,17 +4,12 @@ from bot import TelegramBotFramework
 
 def main():
     # Load environment variables
-    load_dotenv()
+    load_dotenv(override=True)
     
-    # # Get bot token from environment but overwrite it if it is provided inside .env file
-    # load_dotenv(override=True)
-    # bot_token = os.getenv("DEFAULT_BOT_TOKEN")
-    # if not bot_token:
-    #     raise ValueError("DEFAULT_BOT_TOKEN not found in environment variables")
-
-    # Initialize and run the bot
-    # bot = TelegramBotFramework(bot_token)
-    bot = TelegramBotFramework()
+    # You may set bot token from superclass or let the baseclass get itself from environment 
+    bot_token = os.getenv("DEFAULT_BOT_TOKEN", None) 
+    
+    bot = TelegramBotFramework(bot_token)
     bot.run()
 
 if __name__ == "__main__":
