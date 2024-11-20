355a52481a7b88b76f0a148f59294603de569a58
gerson <gerson.freire@gmail.com>
2024-11-20 20:46:16 -0300

    Bump version to 0.4.30 in project files and update core version string

--- src/bot/core.py
+++ src/bot/core.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "0.4.29 command that executes exec AND eval(expression) function"
+__version__ = "0.4.30 command that executes exec AND eval(expression) function"
 
 """TODO's:
 full command line on show version and post init only for admins
