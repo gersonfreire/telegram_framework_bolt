205da8cc7df89c0598c0ebf3399e1a921f62eee4
gerson <gerson.freire@gmail.com>
2024-11-21 19:32:54 -0300

    Bump version to 0.4.32 in project files and update version string in core.py

--- src/bot/core.py
+++ src/bot/core.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "0.4.31 set bot data item"
+__version__ = "0.4.32 set bot data item"
 
 """TODO's:
 full command line on show version and post init only for admins
