39838470fa047e3e0f8c2736075a2c969fd54643
gerson <gerson.freire@gmail.com>
2024-11-22 22:32:21 -0300

    Bump version to 0.4.42 and update version references in pyproject.toml, setup.py, and core.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tlgbotfwk',
-    version='0.4.41',  # Incremented version
+    version='0.4.42',  # Incremented version
     description='A powerful and extensible Python-based Telegram bot framework',
     author='Maker',
     author_email='youremail@example.com',
