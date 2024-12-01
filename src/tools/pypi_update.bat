be1adf4ba0a77cc809e6756ab14306b118e0bd75
gerson <gerson.freire@gmail.com>
2024-12-01 13:26:02 -0300

    feat: Add pypi_update.bat script for automated PyPI uploads

---
+++ src/tools/pypi_update.bat
@@ -0,0 +1,19 @@
+@echo on
+
+cd ..\..
+@REM pause
+
+call .venv\Scripts\activate.bat
+@REM pause
+
+echo Running setup.py
+python setup.py sdist bdist_wheel
+@REM pause
+
+echo Loading environment variables from .env file
+dotenv -f .\.env
+
+echo Uploading to PyPI
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/* --non-interactive --username __token__ --password %PYPI_API_TOKEN%
+
+pause
