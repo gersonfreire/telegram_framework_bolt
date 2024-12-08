53732d0b9eddee8c9c61fc1ba7b28fba11b2a105
Gerson <gerson.freire@gmail.com>
2024-12-08 10:52:53 -0300

    fix: Enhance hostwatch.bat script for improved execution in Windows environment

--- src/examples/hostwatch.bat
+++ src/examples/hostwatch.bat
@@ -1,6 +1,8 @@
 
 @REM This script is used to run the bot in the Windows environment
 
+@echo on
+
 @REM Go to current directory
 cd %~dp0
 
@@ -8,6 +10,9 @@ cd ..\..\
 
 @REM Activate the virtual environment and run the bot
 call .venv\Scripts\Activate
+
+cd 
+@REM c:\Users\gerson\source\repos\pvt\telegram_framework_bolt
 
 @REM Run the bot script repassing the arguments
 python -m src.examples.host_watch_bot %*
